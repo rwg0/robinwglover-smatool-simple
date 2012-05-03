67d1e28b22b8f34e30cba1b041dae6d5e2a6f160
robin <robin@localhost>
2012-05-03 17:39:19 +0100

    Try to improve behaviour of smatool at night in live mode, allow BT connection to drop and reconnect in the morning, hopefully no more lost data in the morning fix locking in cc.pl, other tweaks add graphs script

--- scripts/cc.pl
+++ scripts/cc.pl
@@ -8,18 +8,20 @@ use IO::Select;
 use threads;
 use threads::shared;
 
-my $import :shared= "U";
-my $abs :shared= "U";
+my $import :shared= "0";
+my $abs :shared= "0";
 my $panel2 :shared= "U";
 my $gen :shared = "0";
 my $gentotal :shared = "U";
 my $importtotal :shared= "U";
 my $temp :shared = "U";
+my $lock : shared = "lock";
 
 sub processCCLine {
 	(my $line) = @_;
 #	print "$line\n";
     	if ($line =~ m!<time>([0-9:]+)</time>.*<tmpr> *([\-\d.]+)</tmpr>.*<sensor>(\d+)</sensor>.*(?:<ch1><watts>0*(\d+)</watts></ch1>|<imp>(\d+)</imp>)!) {
+		lock($lock);
         	my $time = $1;
 	        my $watts = $4;
         	$temp = $2;
@@ -30,7 +32,7 @@ sub processCCLine {
         	        $import = $watts;
 	        }
 	        if ($sensor == "1") {
-        	        $abs = $watts;
+        	        $abs = $watts * 1.1; # fudge for inaccuracy of the clamp sensor
 	        }
         	if ($sensor == "2") {
                 	$panel2 = $watts;
@@ -46,10 +48,9 @@ sub processSolarLine {
 	(my $line) = @_;
 #    	print $line;
     	if ($line =~ m!Power=([0-9]+).*Total=([0-9\.]+).*!) {
+		lock($lock);
 		my $power = $1;
         	my $total = $2;
-		$gen = $power;
-		$gentotal = $total;
 		$gen = $power;
 		$gentotal = $total * 1000;
 #        	print "$power $total\n";
@@ -68,14 +69,16 @@ sub readFromPort {
 		if (@ready_clients) {
 			foreach my $fh (@ready_clients)  {
 				if($fh == $sock)  {
-	    				chomp(my $line=<$sock>);
-				        $limit = $process->($line);
+					my $line=<$sock>;
+					if ($line)
+					{
+		    				chomp($line);
+					        $limit = $process->($line);
+					}
 				}
 			}
 		} else {
-			$gen = "U";
-			$gentotal = "U";
-			die "Read timeout.";
+			die "Read timeout from port.";
 		}
 	}
 }
@@ -91,7 +94,10 @@ sub monitor
 			die "Could not create socket: $!\n" unless $sock;
 			readFromPort($sock, $process);
 		} or do {
-			print "failed with error $@\n";
+			my $time = localtime;
+			my $err = $@;
+			chomp($err);
+			print "$time: Monitoring PORT=$PORT: failed with error: '$err'\n";
 			sleep 15;
 		}
 	}
@@ -105,7 +111,21 @@ threads->create( sub {monitor($HOST, "1099", \&processCCLine);});
 while (1)
 {
 	sleep 5;
-        system("rrdtool update powertemp.rrd N:$import:$abs:$gen:$panel2:$temp:$gentotal:$importtotal");
-#        print("rrdtool update powertemp.rrd N:$import:$abs:$gen:$panel2:$temp:$gentotal:$importtotal\n");
+	{
+		lock($lock);		
+		my $i = $import;
+		my $a = $abs;
+		my $g = $gen;
+		my $p2 = $panel2;
+		my $t = $temp;
+		my $gt = $gentotal;
+		my $it = $importtotal;
+		if ($i > 0 && $i > $a * 4)
+		{
+			$i = $a; # filter out spikes in the optismart reading
+		}
+        	system("rrdtool update powertemp.rrd N:$i:$a:$g:$p2:$t:$gt:$it");
+#        	print("rrdtool update powertemp.rrd N:$i:$a:$g:$p2:$t:$gt:$it\n");
+	}
 
 }
